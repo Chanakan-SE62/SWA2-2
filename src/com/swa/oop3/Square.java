1b08833ea068fb1ee136e330c073693e7479d1d4
chanakan <624259009@webmail.npru.ac.th>
2021-07-12 18:11:42 +0700

    Initial commit

---
+++ src/com/swa/oop3/Square.java
@@ -0,0 +1,18 @@
+package com.swa.oop3;
+
+public class Square extends Shape {
+
+	private double width;
+    private double height;
+
+    public void setValues(double width, double height) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getArea() {
+        this.area = this.height * this.width;
+        return this.area;
+	}
+
+}
---
+++ src/com/swa/oop3/Triangle.java
@@ -0,0 +1,17 @@
+package com.swa.oop3;
+
+public class Triangle extends Shape{
+	private double base;
+	private double height;
+	
+	public void setValues(double base, double height) {
+		this.height = height;
+		this.base = base;
+	}
+	
+	public double getArea() {
+		this.area = (this.height * this.base) / 2;
+		return this.area;
+	}
+
+}
